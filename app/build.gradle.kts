47a89d6db8fb82c4ef8f5afa34248045941d27ff
Asya-67 <annadorovskikh111@gmail.com>
2025-07-24 18:58:57 +0300

    last commit 24.07

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,10 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation ("info.picocli:picocli:4.7.7")
+    annotationProcessor ("info.picocli:picocli-codegen:4.7.7")
+    implementation("info.picocli:picocli:4.7.5")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
 }
 
 tasks.test {
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,68 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+
+@Command(
+        name = "App",
+        mixinStandardHelpOptions = true,
+        version = "App 1.0",
+        description = "Compares two configuration files and shows a difference."
+)
+
+public class App implements Runnable {
+
+    @Parameters(
+            index = "0",
+            paramLabel = "filepath1",
+            description = "path to first file"
+    )
+    private String filePath1;
+
+    @Parameters(
+            index = "1",
+            paramLabel = "filepath2",
+            description = "path to second file"
+    )
+    private String filePath2;
+
+    @Option(
+            names = {"-f", "--format"},
+            description = "output format [default: ${DEFAULT-VALUE}]",
+            defaultValue = "stylish"
+    )
+    private String format;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+
+    }
+    @Override
+    public void run() {
+        try {
+            Map<String, Object> data1 = parse(filePath1);
+            Map<String, Object> data2 = parse(filePath2);
+            System.out.println("File 1 content: " + data1);
+            System.out.println("File 2 content: " + data2);
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+    private static Map<String, Object> parse(String filePath) throws Exception{
+        String content = Files.readString(Paths.get(filePath));
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(content, Map.class);
     }
 }
